b74b6566b41b82bf92539c572d23793b09f71772
Ethan <shenxd1989@163.com>
2015-08-04 10:14:08 +0800

    version1.1
    delete some header files

--- header.h
+++ header.h
@@ -2,21 +2,13 @@
 
 #define HEADER_H
 
-#include "minmax.h"
 #include <vector>
 #include <list>
 #include <iterator>
 #include <algorithm>
-#include <atlimage.h>
-#include <time.h>
 #include <math.h>
 #include <string>
 #include <sstream>
-#include "io.h"
-
-
-
-
 #include <iostream>
 #include <fstream>
 
@@ -26,23 +18,23 @@
 #include "../opencv/include/opencv/cxcore.h"
 
 
-#ifdef DEBUG
+#ifdef _DEBUG
 
 #pragma comment(lib, "../opencv/lib/opencv_core248d.lib")
 #pragma comment(lib, "../opencv/lib/opencv_objdetect248d.lib")
 #pragma comment(lib, "../opencv/lib/opencv_highgui248d.lib")
 #pragma comment(lib, "../opencv/lib/opencv_imgproc248d.lib")
 
-#endif
 
-#ifndef DEBUG
+
+#else
 
 #pragma comment(lib, "../opencv/lib/opencv_core248.lib")
 #pragma comment(lib, "../opencv/lib/opencv_objdetect248.lib")
 #pragma comment(lib, "../opencv/lib/opencv_highgui248.lib")
 #pragma comment(lib, "../opencv/lib/opencv_imgproc248.lib")
 
-#endif
 
 #endif
 
+#endif
